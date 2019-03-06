fc44d522fc0bcd4adb4b44ccd5cf114112f029a5
Ashley Barreto <47192330+GeekeryGaming@users.noreply.github.com>
2019-03-06 12:39:39 -0500

    Stuff for Apocalyptic game
    This is the updated stuff for my apocalyse gme, "Spoopy Valley"

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 playerPos;
+    public Transform destination;
+    public Transform hazard;
+    private Rigidbody pRB;
+
+    // Start is called before the first frame update
+    void Start(){
+        pRB = GetComponent<Rigidbody>();
+        playerPos = transform.position;
+        }
+
+    // Update is called once per frame
+    void Update(){
+        //transform.position = playerPos;
+
+        if (Input.GetKey(KeyCode.W))
+            {
+            pRB.AddForce(Vector3.left*10);
+            //playerPos += Vector3.left;
+           // playerPos -= transform.right;
+        }
+        
+        if (playerPos.x == destination.position.x &&
+            playerPos.z == destination.position.z){
+            playerPos += transform.up;
+        }
+        if (playerPos.x == hazard.position.x &&
+            playerPos.z == hazard.position.z)
+        {
+            playerPos -= transform.up;
+        }
+
+
+        transform.position = playerPos;
+    }
+
+}
+
---
+++ Assets/Scripts/player1.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour
+{
+    public float speed;
+    private Vector3 playerPos;
+    private Rigidbody playerRigidbody;
+    public bool floorContact;
+    private object playerRigidboy;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigidbody.AddForce(Vector3.left * speed*Time.deltaTime);
+
+        }if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.AddForce(Vector3.forward * speed*Time.deltaTime);
+
+        }if (Input.GetKey(KeyCode.S))
+        {
+            playerRigidbody.AddForce(Vector3.right * speed*Time.deltaTime);
+
+        }if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.AddForce(Vector3.back * speed*Time.deltaTime);
+            //playerRigidbody.AddTorque(Vector3.down);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) &&floorContact)
+        {
+            playerRigidbody.AddForce(Vector3.up * (speed*0.3f) * Time.deltaTime,ForceMode.Impulse);
+        }
+
+        if (transform.position.y < -10)
+        {
+            transform.position = playerPos;
+        }
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            floorContact = true;
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            floorContact = false;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("Congratulation Loser");
+            text.SetActive(true);
+        }
+    }
+}
